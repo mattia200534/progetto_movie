72766953969c61c821a5521e422868c5079d6b8c
Verri Mattia <mattiaverri05@gmail.com>
2025-11-28 12:49:05 +0100

    "Homeview pt.1 completata"

---
+++ lib/views/home_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:movie/models/movie.dart';
+import 'package:movie/viewmodels/movie_view_model.dart';
+import 'package:provider/provider.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<MovieViewModel>().fectMovies();
+    });
+    return Scaffold(
+      appBar: AppBar(title: Text("Movie Collection APP"), centerTitle: true),
+      body: Consumer<MovieViewModel>(
+        builder: (context, viewmodel, child) {
+          if (viewmodel.isloading) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (viewmodel.movies.isEmpty) {
+            return const Center(
+              child: Text(
+                "Non hai inserito alcun film",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24),
+              ),
+            );
+          }
+          return const Text("verrà sostituito dalla lista dei film");
+        },
+      ),
+    );
+  }
+}
